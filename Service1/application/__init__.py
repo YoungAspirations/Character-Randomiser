f7604eef9d6f28faf96bec74ceff4e7908cf7ffe
Christopher Pierre-Samuel <c.pierre-samuel@hotmail.co.uk>
2022-05-16 22:38:04 +0100

    app adjustments

--- Service1/application/__init__.py
+++ Service1/application/__init__.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request, url_for, Flask
+from flask import render_template, Flask
 import uuid, requests
 from random import choice
 
@@ -7,11 +7,11 @@ app.config['SECRET_KEY'] = str(uuid.uuid4)
 
 @app.route('/Character', methods=['GET'])
 def Character():
-    race = requests.get('http://url/race')
-    role = requests.get('http://url/role')
-    affinities = {'Neutral':'0', 'Good':'5', 'Bad':'5'}
-    Alignment=
-    return render_template(url_for('Character'), race='race', role='role', alignment ='Alignment')
+    race = requests.get('http://localhost:5001/race')
+    role = requests.get('http://localhost:5002/role')
+    affinity = requests.get('http://localhost:5003/Character')
+    
+    return render_template('Character.html', alignment= affinity)
 
 if __name__ == '__main__':
     app.run(port=5002, debug=True, host='0.0.0.0')
--- Service4/application/__init__.py
+++ Service4/application/__init__.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request, url_for, Flask
+from flask import render_template, redirect, request, url_for, Flask, Response
 import uuid, requests
 from random import choice
 
@@ -7,11 +7,11 @@ app.config['SECRET_KEY'] = str(uuid.uuid4)
 
 @app.route('/Character', methods=['GET'])
 def Character():
-    race = requests.get('http://url/race')
-    role = requests.get('http://url/role')
+    race = requests.get('http://localhost:5001/race')
+    role = requests.get('http://localhost:5002/role')
     affinities = {'Neutral':'0', 'Good':'5', 'Bad':'5'}
-    Alignment=
-    return render_template(url_for('Character'), race='race', role='role', alignment ='Alignment')
+    Alignment=sum()
+    return Response()
 
 if __name__ == '__main__':
-    app.run(port=5002, debug=True, host='0.0.0.0')
+    app.run(port=5003, debug=True, host='0.0.0.0')
