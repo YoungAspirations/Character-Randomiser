8f548b9aad60bdb427a8df9417c44a53eced5a6d
Christopher Pierre-Samuel <c.pierre-samuel@hotmail.co.uk>
2022-05-17 16:36:20 +0100

    This is long

--- Service1/application/__init__.py
+++ Service1/application/__init__.py
@@ -1,15 +1,25 @@
 from flask import render_template, Flask
-import uuid, requests
+import uuid, requests, json
 from random import choice
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = str(uuid.uuid4)
 
+@app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
 def Character():
+    #race = requests.get('http://localhost:5001/race').text
+    #role = requests.get('http://localhost:5002/role').text
+    #components = race.update(role)
+    #affinity = requests.post('http://localhost:5003/Character', data=components).text
+
     race = requests.get('http://localhost:5001/race').text
     role = requests.get('http://localhost:5002/role').text
-    components = race.update(role)
+    print(race)
+    print(role)
+    race_dict = json.loads(race)
+    role_dict = json.loads(role)
+    components = dict(race_dict.items + role_dict.items())
     affinity = requests.post('http://localhost:5003/Character', data=components).text
     
     return render_template('Character.html',race = race, role = role, alignment = affinity)
--- Service2/application/__init__.py
+++ Service2/application/__init__.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Flask, Response
 from random import choice
 app = Flask(__name__)
