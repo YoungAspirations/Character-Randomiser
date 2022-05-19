adf0fc01b46d1be531b23fef7b64fc276fe38c32
Christopher Pierre-Samuel <c.pierre-samuel@hotmail.co.uk>
2022-05-19 09:24:08 +0000

    a

--- Service1/testing/test_s1.py
+++ Service1/testing/test_s1.py
@@ -11,18 +11,19 @@ class TestBase(TestCase):
 
 class TestView(TestBase):
     def test_Character_get(self):
-        response = self.client.get(url_for('Character'))
-        self.assertEqual(response.status_code, 200)
+        response = self.client.get(url_for('Character', json={("Human", 0), ("Priest", 3)}))
+        self.assertEqual(response.status_code, 200) #Sort this out if there's time
 
 class TestGet(TestBase):
     def test_get_race_role(self):
         with requests_mock.Mocker() as m:
             m.get('http://localhost:5001/get/race', json={"Human": 0})
             m.get('http://localhost:5002/get/role', json={"Priest": 3})
+            m.post('http://localhost:5003/post/Character', json={"key": {"Human": 0}, "value": {"Priest": 3}})
             response = self.client.get(url_for('Character'))
-            #self.assertIn(b'0', response.data)
+            self.assertIn(b'Human', response.data)
             #self.assertIn(b'Priest', response.data)
-            assert Character()["race"] == '{"Human": 0}'
+            #assert Character()["race"] == '{"Human": 0}'
 # class TestPost(TestBase):
 #     def test_post_alignment:
 #         with requests_mock.Mocker() as m:
