570f87dccea792ed73b92808addab5db934537ba
Christopher Pierre-Samuel <c.pierre-samuel@hotmail.co.uk>
2022-05-19 09:21:03 +0000

    updated tests more than halfway through

---
+++ Service4/testing/test_s4.py
@@ -0,0 +1,22 @@
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+#from random import choice
+from application import Character, app
+import requests_mock, json
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestView(TestBase):
+    def test_Character_get(self):
+        response = self.client.post(url_for('Character'),json={"key": {"Human", 0}, "value": {"Priest", 3}})
+        self.assertEqual(response.status_code, 200)
+
+class TestPost(TestBase):
+    def test_post_alignment:
+        with requests_mock.Mocker() as m:
+            m.get('http://localhost:5003/post/Character', json={"key":{"Human": 0}, "value":{"Priest": 3}})
+            response = self.client.post(url_for('Character')) #data = 'jsonobject')
+            assert json.loads()
